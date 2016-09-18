2dd413f0a5c6c19dfc200edda65094a35c5ff099
Gabriel25 <barnoth.g@gmail.com>
2016-09-18 14:11:13 +0200

    Scene2d.ui test

--- core/src/com/adamcomp/pclevel/Main.java
+++ core/src/com/adamcomp/pclevel/Main.java
@@ -3,22 +3,46 @@ package com.adamcomp.pclevel;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Main extends Game {
-    public static final float PPM = 100;
-    public static final int V_WIDTH = 640;
-    public static final int V_HEIGHT = 480;
-    public SpriteBatch batch;
+    private Stage stage;
+    private Skin skin;
+    private Window wnd;
 
     @Override
     public void create () {
-        batch = new SpriteBatch();
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        skin = new Skin(Gdx.files.internal("visui/uiskin.json"),
+                new TextureAtlas("visui/uiskin.atlas"));
+
+        wnd = new Window("Objects", skin);
+        stage.addActor(wnd);
+        wnd.debug();
+        wnd.setPosition(10, 10);
+        wnd.setSize(150, 500);
+        TextButton btn = new TextButton("TEST", skin);
+        wnd.add().setActor(btn);
     }
 
     @Override
-    public void render () {
-        super.render();
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+        stage.act(Gdx.graphics.getDeltaTime());
+
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.draw();
     }
 }
