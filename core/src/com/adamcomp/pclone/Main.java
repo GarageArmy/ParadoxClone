7b654b598f33300fcd1520a6d32ea8f2b6eec2e5
Aduci01 <zuzzu01@gmail.com>
2016-10-22 11:42:36 +0200

    basic physics

--- core/src/com/adamcomp/pclone/Main.java
+++ core/src/com/adamcomp/pclone/Main.java
@@ -5,25 +5,30 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.uwsoft.editor.renderer.SceneLoader;
+import com.uwsoft.editor.renderer.physics.PhysicsBodyLoader;
 import com.uwsoft.editor.renderer.utils.ItemWrapper;
 
 public class Main extends ApplicationAdapter {
 	SceneLoader sceneLoader;
+	Player player;
+	Viewport viewport;
+
 	@Override
 	public void create () {
-		Viewport viewport = new FitViewport(300, 200);
+		viewport = new FitViewport(320, 240);
 		sceneLoader = new SceneLoader();
 		sceneLoader.loadScene("MainScene", viewport);
 
-		Player player = new Player();
 		ItemWrapper root = new ItemWrapper(sceneLoader.getRoot());
 
+		player = new Player(sceneLoader.world);
 		root.getChild("player").addScript(player);
+
 	}
 
 	@Override
@@ -31,5 +36,8 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		sceneLoader.getEngine().update(Gdx.graphics.getDeltaTime());
+
+
+		((OrthographicCamera)viewport.getCamera()).position.x = player.getX() + player.getWidth() / 2;
 	}
 }
--- core/src/com/adamcomp/pclone/Player.java
+++ core/src/com/adamcomp/pclone/Player.java
@@ -3,8 +3,15 @@ package com.adamcomp.pclone;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.RayCastCallback;
+import com.badlogic.gdx.physics.box2d.World;
+import com.uwsoft.editor.renderer.components.DimensionsComponent;
 import com.uwsoft.editor.renderer.components.TransformComponent;
+import com.uwsoft.editor.renderer.components.physics.PhysicsBodyComponent;
+import com.uwsoft.editor.renderer.physics.PhysicsBodyLoader;
 import com.uwsoft.editor.renderer.scripts.IScript;
 import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 
@@ -14,25 +21,116 @@ import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 public class Player implements IScript {
     private Entity player;
     private TransformComponent transformComponent;
+    private DimensionsComponent dimensionsComponent;
 
+    private World world;
+
+    private Vector2 speed;
+    private float gravity = -500;
+    private float jumpSpeed = 200;
+
+
+
+
+
+    public Player (World world){
+        this.world = world;
+    }
 
     @Override
     public void init(Entity entity) {
         player = entity;
         transformComponent = ComponentRetriever.get(entity, TransformComponent.class);
+        dimensionsComponent = ComponentRetriever.get(entity, DimensionsComponent.class);
+
+        speed = new Vector2(100, 0);
     }
 
     @Override
     public void act(float delta) {
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
-            transformComponent.x -= 10;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-            transformComponent.x += 10;
+        inputHandler(delta);
+
+        speed.y += gravity * delta;
+        transformComponent.y += speed.y * delta;
+
+        rayCast();
     }
 
     @Override
     public void dispose() {
 
+    }
+
+    private void inputHandler(float delta){
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            transformComponent.x -= speed.x * delta;
+            transformComponent.scaleX = -1.0f;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            transformComponent.x += speed.x * delta;
+            transformComponent.scaleX = 1.0f;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            speed.y = jumpSpeed;
+        }
+    }
+
+    private void rayCast() {
+        float rayGap = dimensionsComponent.height / 2;
+        float raySize = -(speed.y) * Gdx.graphics.getDeltaTime();
+
+
+        //Raycast down
+        if (speed.y <= 0) {
+            Vector2 rayFrom = new Vector2((transformComponent.x + dimensionsComponent.width / 2) * PhysicsBodyLoader.getScale(),
+                    (transformComponent.y + rayGap) * PhysicsBodyLoader.getScale());
+            Vector2 rayTo = new Vector2((transformComponent.x + dimensionsComponent.width / 2) * PhysicsBodyLoader.getScale(),
+                    (transformComponent.y - raySize) * PhysicsBodyLoader.getScale());
+
+            //Cast the ray
+            world.rayCast(new RayCastCallback() {
+                @Override
+                public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
+                    speed.y = 0;
+                    transformComponent.y = point.y / PhysicsBodyLoader.getScale() + 0.01f;
+                    return 0;
+
+                }
+            }, rayFrom, rayTo);
+        }
+
+        //raycast up
+        if (speed.y > 0) {
+            Vector2 rayFrom = new Vector2((transformComponent.x + dimensionsComponent.width / 2) * PhysicsBodyLoader.getScale(),
+                    (transformComponent.y + rayGap) * PhysicsBodyLoader.getScale());
+            Vector2 rayTo = new Vector2((transformComponent.x + dimensionsComponent.width / 2) * PhysicsBodyLoader.getScale(),
+                    (transformComponent.y + raySize + rayGap + rayGap) * PhysicsBodyLoader.getScale());
+
+            //Cast the ray
+            world.rayCast(new RayCastCallback() {
+                @Override
+                public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
+                    speed.y = 0;
+                    return 0;
+
+                }
+            }, rayFrom, rayTo);
+        }
+    }
+
+    public float getX (){
+        return transformComponent.x;
+    }
+    public float getWidth (){
+        return dimensionsComponent.width;
+    }
+    public float getY (){
+        return transformComponent.y;
+    }
+    public float getHeight (){
+        return dimensionsComponent.height;
     }
 }
